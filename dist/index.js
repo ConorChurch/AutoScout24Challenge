cf6f93455c085ca28bcefbabb5a2b7ca2284f00e
Conor <conorchurch42@gmail.com>
2021-05-22 01:22:45 +0100

    Second Commit - Finishing 4th Requirement and fixing up some outputs of previous requirements

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-//import html from 'web-ui.html';
-//import * as csvParse from 'csv-parser';
-//var myParser:csvParse.CsvParser = csvParse({delimiter: ','}, function(data: any, err: any) {
-//	console.log(data);
-//}) as csvParse.CsvParser;
 const app = express_1.default();
 const csv = require('csv-parser');
 const fs = require('fs');
@@ -18,13 +13,11 @@ fs.createReadStream('listings.csv')
     .pipe(csv())
     .on('data', (data) => results.push(data))
     .on('end', () => {
-    //    console.log(results);
 });
 fs.createReadStream('contacts.csv')
     .pipe(csv())
     .on('data', (data) => results2.push(data))
     .on('end', () => {
-    //    console.log(results);
 });
 function averageSellerPrice() {
     var privatePrice = 0;
@@ -48,8 +41,13 @@ function averageSellerPrice() {
             totalOther++;
         }
     }
-    const test = [privatePrice / totalPrivate, dealerPrice / totalDealer, otherPrice / totalOther];
-    return test;
+    var array = [];
+    array.push({
+        private: Math.round(privatePrice / totalPrivate),
+        dealer: Math.round(dealerPrice / totalDealer),
+        other: Math.round(otherPrice / totalOther)
+    });
+    return array;
 }
 function distributionOfCarsByMake() {
     var totalNumberOfCars = 0;
@@ -68,7 +66,7 @@ function distributionOfCarsByMake() {
     for (let entry of map2.entries()) {
         array.push({
             name: entry[0],
-            value: (entry[1] / totalNumberOfCars) * 100
+            value: Math.round((entry[1] / totalNumberOfCars) * 100)
         });
     }
     var sorted = array.sort(function (a, b) {
@@ -125,14 +123,77 @@ function averagePriceOfContactedListings() {
         }
     }
     //Need to round result before sending back
-    return (totalPriceOfThirty / numberOfListingsInThirtyPercent).toString();
+    return Math.round(totalPriceOfThirty / numberOfListingsInThirtyPercent).toString();
 }
-function top5listingsPerMonth() {
+function top5listingsPerMonth(getMonth) {
+    var map2 = new Map();
+    var pair = [];
+    for (let i in results2) {
+        const numDate = +Object.values(results2[i])[1];
+        const date = new Date(numDate);
+        if (map2.has(Object.values(results2[i])[0]) == false) {
+            map2.set(Object.values(results2[i])[0], [date.getUTCMonth(), 1]);
+        }
+        else if (map2.has(Object.values(results2[i])[0]) == true) {
+            map2.set(Object.values(results2[i])[0], [date.getUTCMonth(), map2.get(Object.values(results2[i])[0])[1] + 1]);
+        }
+    }
+    var array = [];
+    for (let entry of map2.entries()) {
+        array.push({
+            id: entry[0],
+            make: "",
+            month: entry[1][0],
+            price: "",
+            mileage: "",
+            value: entry[1][1]
+        });
+    }
+    var sorted = array.sort(function (a, b) {
+        return (a.value < b.value) ? 1 : ((b.value < a.value) ? -1 : 0);
+    });
+    var top5 = [];
+    var count = 0;
+    for (let i of sorted) {
+        if (i.month == getMonth && count < 5) {
+            top5[count] = i;
+            count++;
+        }
+    }
+    for (let i in results) {
+        if (Object.values(results[i])[0] == top5[0].id) {
+            top5[0].make = Object.values(results[i])[1];
+            top5[0].price = Object.values(results[i])[2];
+            top5[0].mileage = Object.values(results[i])[3];
+        }
+        else if (Object.values(results[i])[0] == top5[1].id) {
+            top5[1].make = Object.values(results[i])[1];
+            top5[1].price = Object.values(results[i])[2];
+            top5[1].mileage = Object.values(results[i])[3];
+        }
+        else if (Object.values(results[i])[0] == top5[2].id) {
+            top5[2].make = Object.values(results[i])[1];
+            top5[2].price = Object.values(results[i])[2];
+            top5[2].mileage = Object.values(results[i])[3];
+        }
+        else if (Object.values(results[i])[0] == top5[3].id) {
+            top5[3].make = Object.values(results[i])[1];
+            top5[3].price = Object.values(results[i])[2];
+            top5[3].mileage = Object.values(results[i])[3];
+        }
+        else if (Object.values(results[i])[0] == top5[4].id) {
+            top5[4].make = Object.values(results[i])[1];
+            top5[4].price = Object.values(results[i])[2];
+            top5[4].mileage = Object.values(results[i])[3];
+        }
+    }
+    return top5;
 }
 app.get('/', (req, res) => {
     // res.send(averageSellerPrice());
     //   res.send(distributionOfCarsByMake());
     res.send(averagePriceOfContactedListings());
+    //   res.send(top5listingsPerMonth(3));
 });
 //app.get('/display', (req: any, res) => {
 //	res.render("web-ui");
--- index.ts
+++ index.ts
@@ -3,15 +3,7 @@ import path from "path";
 export interface IHash {
 	[key: string] : number;
 }
-//import html from 'web-ui.html';
-//import * as csvParse from 'csv-parser';
 
-
-
-
-//var myParser:csvParse.CsvParser = csvParse({delimiter: ','}, function(data: any, err: any) {
-//	console.log(data);
-//}) as csvParse.CsvParser;
 
 const app = express();
 const csv = require('csv-parser');
@@ -23,19 +15,17 @@ fs.createReadStream('listings.csv')
   .pipe(csv())
   .on('data', (data: any) => results.push(data))
   .on('end', () => {
-  //    console.log(results);
-  });
+});
 
 fs.createReadStream('contacts.csv')
   .pipe(csv())
   .on('data', (data: any) => results2.push(data))
   .on('end', () => {
-  //    console.log(results);
 });
 
 
 
-function averageSellerPrice(): Number[]{ 
+function averageSellerPrice(): Object[]{ 
    var privatePrice: number = 0;
    var dealerPrice: number = 0;
    var otherPrice: number = 0;
@@ -60,9 +50,15 @@ function averageSellerPrice(): Number[]{
 	}
    }
 
-   const test: Number[] = [privatePrice/totalPrivate, dealerPrice/totalDealer, otherPrice/totalOther];
+   var array = [];
 
-   return test;
+   array.push({
+            private: Math.round(privatePrice/totalPrivate),
+	    dealer: Math.round(dealerPrice/totalDealer),
+            other: Math.round(otherPrice/totalOther)
+   });
+
+   return array;
 }
 
 
@@ -90,7 +86,7 @@ function distributionOfCarsByMake(): Object[] {
 
    	array.push({
 		name: entry[0],
-		value: (entry[1]/totalNumberOfCars)*100
+		value: Math.round((entry[1]/totalNumberOfCars)*100)
 	});
    }
 
@@ -169,17 +165,83 @@ var totalNumberOfListings: number = 0;
      }
     }
     //Need to round result before sending back
-    return (totalPriceOfThirty/numberOfListingsInThirtyPercent).toString();
+    return Math.round(totalPriceOfThirty/numberOfListingsInThirtyPercent).toString();
 }
 
 
 
-function top5listingsPerMonth(): String[]{
+function top5listingsPerMonth(getMonth: number): Object[]{
+	var map2 = new Map();
+	var pair = [];
+	for(let i in results2){
+		const numDate = +Object.values(results2[i])[1];
+		const date = new Date(numDate);
+		if(map2.has(Object.values(results2[i])[0]) == false){
+			
+			map2.set(Object.values(results2[i])[0], [date.getUTCMonth(), 1]);
 
-	
-	
+        	}
+		else if(map2.has(Object.values(results2[i])[0]) == true){
+			
+                	map2.set(Object.values(results2[i])[0], [date.getUTCMonth(), map2.get(Object.values(results2[i])[0])[1]+1]);
+        	}
+   	}
+	var array = [];
+        for( let entry of map2.entries()){
+     	   array.push({
+	   	id: entry[0],
+	   	make: "",
+		month: entry[1][0],
+		price: "",
+		mileage: "",
+                value: entry[1][1]
+     	   });
+        }
+        var sorted = array.sort(function(a, b) {
+	   return (a.value < b.value) ? 1 : ((b.value < a.value) ? -1 : 0)
+	});
 
+	var top5: any = [];
+	var count: number = 0;
+	for(let i of sorted){
 
+		if(i.month == getMonth && count < 5){
+			top5[count] = i;
+			
+			count++;
+		}
+
+	}
+	for(let i in results){
+		if(Object.values(results[i])[0] == top5[0].id){
+			top5[0].make	= Object.values(results[i])[1];
+			top5[0].price = Object.values(results[i])[2];
+			top5[0].mileage = Object.values(results[i])[3];
+		}
+		else if(Object.values(results[i])[0] == top5[1].id){
+                        top5[1].make = Object.values(results[i])[1];
+                        top5[1].price = Object.values(results[i])[2];
+                        top5[1].mileage = Object.values(results[i])[3];
+		}
+                else if(Object.values(results[i])[0] == top5[2].id){
+                        top5[2].make = Object.values(results[i])[1];
+                        top5[2].price = Object.values(results[i])[2];
+                        top5[2].mileage = Object.values(results[i])[3];
+                }
+		else if(Object.values(results[i])[0] == top5[3].id){
+                        top5[3].make = Object.values(results[i])[1];
+                        top5[3].price = Object.values(results[i])[2];
+                        top5[3].mileage = Object.values(results[i])[3];
+                }
+                else if(Object.values(results[i])[0] == top5[4].id){
+                        top5[4].make = Object.values(results[i])[1];
+                        top5[4].price = Object.values(results[i])[2];
+                        top5[4].mileage = Object.values(results[i])[3];
+                }
+
+	}
+
+	return top5;
 }
 
 
@@ -187,6 +249,7 @@ app.get('/', (req, res) => {
 // res.send(averageSellerPrice());
 //   res.send(distributionOfCarsByMake());
     res.send(averagePriceOfContactedListings());
+//   res.send(top5listingsPerMonth(3));
 });
 
 //app.get('/display', (req: any, res) => {
